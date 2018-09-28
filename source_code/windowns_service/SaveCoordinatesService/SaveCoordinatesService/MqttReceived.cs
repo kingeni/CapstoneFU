59feaf32efc2126aedcdb15d5b39a2da761f5700
HUYTDSE61754\Huy Ta <tahuygl@gmail.com>
2018-09-28 18:00:34 +0700

    SaveCoordinatesService

---
+++ source_code/windowns_service/SaveCoordinatesService/SaveCoordinatesService/MqttReceived.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SaveCoordinatesService
+{
+    class MqttReceived
+    {
+        public EventHandler<CommandInfo> MqttReceivedCommand;
+        MqttClient client;
+        string clientID;
+        public MqttReceived(string hostName, string topicHost)
+        {
+            client = new MqttClient(hostName);
+            clientID = Guid.NewGuid().ToString();
+            client.Connect(clientID);
+            ushort msgId = client.Subscribe(new string[] { topicHost },
+            new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            client.MqttMsgSubscribed += Client_MqttMsgSubscribed;
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string message = Encoding.UTF8.GetString(e.Message);
+            string[] dataMessage = message.Split('|');
+            if (MqttReceivedCommand != null)
+            {
+                if (dataMessage.Count() == 2)
+                {
+                    CommandInfo commandInfo = new CommandInfo
+                    {
+                        CommandName = dataMessage[0],
+                        CommandData = dataMessage[1]
+                    };
+                    MqttReceivedCommand(this, commandInfo);
+                }
+
+            }
+            //Console.WriteLine("Received = " + Encoding.UTF8.GetString(e.Message) + " on topic " + e.Topic);
+        }
+
+        private void Client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+        {
+            //Console.WriteLine("Subscribed for id = " + e.MessageId);
+        }
+
+    }
+    public class CommandInfo : EventArgs
+    {
+        public string CommandName { get; set; }
+        public string CommandData { get; set; }
+    }
+}
---
+++ source_code/windowns_service/SaveCoordinatesService/SaveCoordinatesService/Service1.cs
@@ -0,0 +1,77 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SaveCoordinatesService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string BrokerAddress = "broker.hivemq.com";
+
+            MqttClient client = new MqttClient(BrokerAddress);
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            var clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            // whole topic
+            string Topic = "presence";
+            // subscribe to the topic with QoS 2
+            client.Subscribe(new string[] { Topic }, new byte[] { 2 });   // we need arrays as parameters because we can subscribe to different topics with one call
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            //Coordinates|B123:5:6
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            if (ReceivedMessage.Contains("Coordinates"))
+            {
+                string[] ele = ReceivedMessage.Split('|');
+                string info = ele[1];
+                string[] ele1 = info.Split(':');
+                try
+                {
+                    //This is my connection string i have assigned the database file address path  
+                    string MyConnection2 = "datasource=localhost;port=3306;username=root;password=123456;SslMode = none";
+                    //This is my insert query in which i am taking input from the user through windows forms  
+                    string Query = "insert into beacon_webapp.object_coordinates(object_id,x,y) values('" + ele1[0] + "','" + ele1[1] + "','" + ele1[2] + "');";
+                    //This is  MySqlConnection here i have created the object and pass my connection string.  
+                    MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                    //This is command class which will handle the query and connection object.  
+                    MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                    MySqlDataReader MyReader2;
+                    MyConn2.Open();
+                    MyReader2 = MyCommand2.ExecuteReader();     // Here our query will be executed and data saved into the database.  
+                    while (MyReader2.Read())
+                    {
+                    }
+                    MyConn2.Close();
+                }
+                catch (Exception)
+                {
+                    //ingnored
+                }
+            }
+        }
+    }
+}
